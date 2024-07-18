fafe46f2065a973b3449a6f3065242c78720e0dc
Povilas <167984240+Povilas88@users.noreply.github.com>
2024-07-18 08:40:42 +0300

    condensed foul function

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -13,10 +13,10 @@ buttonsDOM.forEach((button, index) => {
     button.addEventListener('click', () => {
         switch (index) {
             case 0:
-                foul(1);
+                foul(1, 'add');
                 break;
             case 1:
-                foulMinus(1);
+                foul(1, 'minus');
                 break;
             case 2:
                 team(1, 1);
@@ -43,10 +43,10 @@ buttonsDOM.forEach((button, index) => {
                 pauseTimer();
                 break;
             case 10:
-                foul(2);;
+                foul(2, 'add');;
                 break;
             case 11:
-                foulMinus(2);;
+                foul(2, 'minus');;
                 break;
             case 12:
                 team(1, 2);
@@ -94,33 +94,30 @@ const bars2DOM = document.querySelectorAll('.bar2');
 let foul1Count = 0;
 let foul2Count = 0;
 
-function foul(team) {
+function foul(team, action) {
     const barsDOM = team === 1 ? bars1DOM : bars2DOM;
-    const foulCount = team === 1 ? foul1Count : foul2Count;
+    let foulCount = team === 1 ? foul1Count : foul2Count;
 
-    if (foulCount < barsDOM.length - 1) {
-        barsDOM[foulCount].style.backgroundColor = 'orange';
-        team === 1 ? foul1Count++ : foul2Count++;
-    } else {
-        for (let i = 0; i < barsDOM.length; i++) {
-            barsDOM[i].style.backgroundColor = 'red';
+    if (action === 'add') {
+        if (foulCount < barsDOM.length - 1) {
+            barsDOM[foulCount].style.backgroundColor = 'orange';
+            team === 1 ? foul1Count++ : foul2Count++;
+        } else {
+            for (let i = 0; i < barsDOM.length; i++) {
+                barsDOM[i].style.backgroundColor = 'red';
+            }
         }
-    }
-}
-
-function foulMinus(team) {
-    const barsDOM = team === 1 ? bars1DOM : bars2DOM;
-    const foulCount = team === 1 ? foul1Count : foul2Count;
-
-    if (foulCount > 0) {
-        for (let i = 0; i < barsDOM.length; i++) {
-            barsDOM[i].style.backgroundColor = 'orange';
+    } else if (action === 'minus') {
+        if (foulCount > 0) {
+            for (let i = 0; i < barsDOM.length; i++) {
+                barsDOM[i].style.backgroundColor = 'orange';
+            }
+            team === 1 ? foul1Count-- : foul2Count--;
         }
-        team === 1 ? foul1Count-- : foul2Count--;
-    }
-    if (foulCount < barsDOM.length) {
-        for (let i = foulCount; i < barsDOM.length; i++) {
-            barsDOM[i].style.backgroundColor = 'transparent';
+        if (foulCount < barsDOM.length) {
+            for (let i = foulCount; i < barsDOM.length; i++) {
+                barsDOM[i].style.backgroundColor = 'transparent';
+            }
         }
     }
 }
